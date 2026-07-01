8f984dc17856e6a02d521d8b03fd3e18b58cfb26
JOHNSON SAIMON KILASI <johnsonsaimon89@users.noreply.github.com>
2026-07-01 11:16:34 +0300

    Converting one Data Type to another using int.parse() and toDouble()

--- Dart/DataType.dart
+++ Dart/DataType.dart
@@ -14,9 +14,28 @@ void main() {
   String myName = "Alice";
   print ("This is String: $myName. This is double: $myHeight. This is int: $myAge. This is bool: $isTrue");
   trial();
-  }
+  Conversion();
+    }
 
   void trial (){
-    print("Is it possible to print without writing a the void main()? 
-    Yes, it is possible to print without writing the void main() function. However, it is not recommended as the main() function is the entry point of a Dart program and is required for proper execution.");
+    print("Is it possible to print without writing a the void main()? Yes, it is possible to print without writing the void main() function. However, it is not recommended as the main() function is the entry point of a Dart program and is required for proper execution.");
   }  
+
+
+  /*Conversion of data types in Dart can be done using the following methods:*/
+  /*Int.parse() converts a string to an interger*/
+  void Conversion(){
+    String age = "24";
+  int ageInt = int.parse (age);
+  print("This is the ageInt: $ageInt");
+
+  /*Now converting a string to a double, this is a little bit different from converting a string to an integer. We use the double.parse() method to convert a string to a double*/
+  String fraction = "3.5";
+  double fractionDouble = double.parse (fraction);
+  print (" This is a string converted to a double: $fractionDouble");
+
+  /*Converting an Interger to a Double*/
+  int myHeight = 67;
+  double myHeightDouble = myHeight.toDouble();
+  print ("This is the myHeightDouble: $myHeightDouble");
+  }
