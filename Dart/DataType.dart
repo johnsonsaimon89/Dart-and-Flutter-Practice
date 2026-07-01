193921530257dd60647d73804f7e9796f412f5ab
JOHNSON SAIMON KILASI <johnsonsaimon89@users.noreply.github.com>
2026-07-01 10:59:22 +0300

    Created variables using different data types

---
+++ Dart/DataType.dart
@@ -0,0 +1,22 @@
+/*Dart program language has several data types. The most common data types include
+bool which accept only two values true and false
+int which stores integer values
+double which has higher precision than int and stores decimal values
+String which stores a sequence of characters*/
+
+/*Below is me trying to use different data types in Dart*/
+
+void main() {
+ 
+  bool isTrue = true;
+  int myAge = 25;
+  double myHeight = 5.9;
+  String myName = "Alice";
+  print ("This is String: $myName. This is double: $myHeight. This is int: $myAge. This is bool: $isTrue");
+  trial();
+  }
+
+  void trial (){
+    print("Is it possible to print without writing a the void main()? 
+    Yes, it is possible to print without writing the void main() function. However, it is not recommended as the main() function is the entry point of a Dart program and is required for proper execution.");
+  }  
